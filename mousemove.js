7910ed7b03166b3920e4085341db8af03f1b7694
KimHyeongJoong <fomula91@gmail.com>
2023-06-25 20:55:39 +0900

    fix: add cursor position

--- mousemove.js
+++ mousemove.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector(".customCursor");
 const moveCursor = (event) => {
   const { clientX: x, clientY: y } = event;
-  cursor.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  cursor.style.transform = `translate3d(${x + 2}px, ${y}px, 0)`;
 };
 
 document.addEventListener("mousemove", moveCursor);
