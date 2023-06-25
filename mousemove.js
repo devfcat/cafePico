b7840af6458cc9f906ff6a58f2c1aa1cb59b1aac
KimHyeongJoong <fomula91@gmail.com>
2023-06-25 13:27:06 +0900

    style: delete main css, fix custom cursor

--- mousemove.js
+++ mousemove.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector(".customCursor");
 const moveCursor = (event) => {
   const { clientX: x, clientY: y } = event;
-  cursor.style.transform = `translate3d(${x - 5}px, ${y - 60}px, 0)`;
+  cursor.style.transform = `translate3d(${x}px, ${y + 5}px, 0)`;
 };
 
 document.addEventListener("mousemove", moveCursor);
